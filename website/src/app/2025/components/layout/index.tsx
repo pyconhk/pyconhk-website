8fb19960285fc49673f6c56c2b24e1aa8f3e4475
Alex Au <alexuau922@gmail.com>
2025-07-20 21:05:17 +0800

    Add Google Analytics integration and update environment variables
    - Added NEXT_PUBLIC_GA_ID to .env and environment.d.ts
    - Included GoogleAnalytics component in layout.tsx
    - Updated package.json and package-lock.json to include @next/third-parties
    - Adjusted opacity in layout component for improved visual consistency
    - Modified overscroll behavior in SponsorChoice component

--- website/src/app/2025/components/layout/index.tsx
+++ website/src/app/2025/components/layout/index.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       }}
     >
       <div className='container mx-auto py-16 px-4 sm:px-6 lg:px-8'>
-        <div className='bg-slate-100 rounded-xl shadow-xl py-12 px-8 md:px-12 md:py-16 lg:px-16 lg:py-24 relative mt-20 text-base/8 md:text-lg/8 text-gray-700 opacity-90'>
+        <div className='bg-slate-100 rounded-xl shadow-xl py-12 px-8 md:px-12 md:py-16 lg:px-16 lg:py-24 relative mt-20 text-base/8 md:text-lg/8 text-gray-700 opacity-80'>
           {children}
         </div>
       </div>
