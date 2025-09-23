2e318064e254270b1b9b2234e3eebf49b7c467b6
Alex Au <alexuau922@gmail.com>
2025-09-23 23:46:21 +0800

    feat(supporting-organizations): update logos to use WebP format and add new organizations

--- website/src/app/2025/supporting-organizations/page.tsx
+++ website/src/app/2025/supporting-organizations/page.tsx
@@ -1,49 +1,91 @@
-import codeaholicsLogoImg from '../../../../public/2025/supporting-organizations/codeaholics.webp';
-import gdgCloudHKLogoImg from '../../../../public/2025/supporting-organizations/gdg-cloud-hk.webp';
-import hkaceLogoImg from '../../../../public/2025/supporting-organizations/hkace.webp';
-import hkucsaLogoImg from '../../../../public/2025/supporting-organizations/hkucsa.webp';
-import polyuRoboticsClubLogoImg from '../../../../public/2025/supporting-organizations/polyu-robotics-club.webp';
-import wtmhkLogoImg from '../../../../public/2025/supporting-organizations/wtmhk.webp';
+import agileHKImg from '../../../../public/2025/supporting-organizations/agile_hk.webp';
+import awsUGHKImg from '../../../../public/2025/supporting-organizations/aws_ug_hk.webp';
+import codeaholicsImg from '../../../../public/2025/supporting-organizations/codeaholics.webp';
+import eduHKMathItImg from '../../../../public/2025/supporting-organizations/eduhk_math_it_dept.webp';
+import gdgCloudHKImg from '../../../../public/2025/supporting-organizations/gdg_cloud_hk.webp';
+import gdgHKImg from '../../../../public/2025/supporting-organizations/gdg_hk.webp';
+import hkaceImg from '../../../../public/2025/supporting-organizations/hkace.webp';
+import japanRPIImg from '../../../../public/2025/supporting-organizations/japan_rpi_ug.webp';
+import pyladiesHKImg from '../../../../public/2025/supporting-organizations/pyladies_hk.webp';
+import pyladiesTokyoImg from '../../../../public/2025/supporting-organizations/pyladies_tokyo.webp';
+import pythonAsiaImg from '../../../../public/2025/supporting-organizations/python_asia.webp';
+import vtcImg from '../../../../public/2025/supporting-organizations/vtc.webp';
 import OrganizationLogo, {
   OrganizationLogoProps,
 } from '../components/utils/OrganizationLogo';
 
 const supportingOrganizations: OrganizationLogoProps[] = [
   {
-    name: 'Codeaholics',
-    logo: codeaholicsLogoImg,
-    logoAlt: 'Codeaholics Logo',
-    href: 'https://www.meetup.com/Codeaholics/',
+    name: 'Agile Hong Kong',
+    logo: agileHKImg,
+    logoAlt: 'Agile Hong Kong Logo',
+    href: 'https://www.meetup.com/agile-hong-kong/',
   },
   {
-    name: 'Computer Science Association, Engineering Society, HKU<br/>香港大學工程學會計算機科學學會',
-    logo: hkucsaLogoImg,
-    logoAlt: 'HKU Computer Science Association Logo',
-    href: 'https://i.cs.hku.hk/~csa/',
+    name: 'AWS User Group Hong Kong',
+    logo: awsUGHKImg,
+    logoAlt: 'AWS User Group Hong Kong Logo',
+    href: 'https://awsug.hk/',
+  },
+  {
+    name: 'Codeaholics',
+    logo: codeaholicsImg,
+    logoAlt: 'Codeaholics Logo',
+    href: 'https://www.facebook.com/codeaholics/',
+  },
+  {
+    name: 'Department of Mathematics and<br/>Information Techonology, The EdUHK',
+    logo: eduHKMathItImg,
+    logoAlt: 'EdUHK Math & IT Dept Logo',
+    href: 'https://www.eduhk.hk/mit/en/',
   },
   {
     name: 'Google Developer Group Cloud Hong Kong',
-    logo: gdgCloudHKLogoImg,
-    logoAlt: 'GDG Cloud HK Logo',
+    logo: gdgCloudHKImg,
+    logoAlt: 'GDG Cloud Hong Kong Logo',
     href: 'https://gdg.community.dev/gdg-cloud-hong-kong/',
   },
   {
-    name: 'HKACE - The Hong Kong Association for Computer Education<br/>香港電腦教育學會',
-    logo: hkaceLogoImg,
+    name: 'Google Developer Group Hong Kong',
+    logo: gdgHKImg,
+    logoAlt: 'GDG Hong Kong Logo',
+    href: 'https://gdg.community.dev/gdg-hong-kong/',
+  },
+  {
+    name: 'HKACE<br/>The Hong Kong Association for Computer Education',
+    logo: hkaceImg,
     logoAlt: 'HKACE Logo',
     href: 'https://www.hkace.org.hk/',
   },
   {
-    name: 'PolyU Robotics Club',
-    logo: polyuRoboticsClubLogoImg,
-    logoAlt: 'PolyU Robotics Club Logo',
-    href: 'https://www.instagram.com/polyurobotics',
+    name: 'Japanese Raspberry Pi Users Group<br/>(Raspberry JAM Tokyo)',
+    logo: japanRPIImg,
+    logoAlt: 'Japanese Raspberry Pi Users Group Logo',
+    href: 'https://www.raspi.jp/',
   },
   {
-    name: 'Women Techmakers Hong Kong',
-    logo: wtmhkLogoImg,
-    logoAlt: 'Women Techmakers Hong Kong Logo',
-    href: 'https://www.facebook.com/WTMHongKong/',
+    name: 'PyLadies Hong Kong',
+    logo: pyladiesHKImg,
+    logoAlt: 'PyLadies Hong Kong Logo',
+    href: 'https://linktr.ee/pyladieshk',
+  },
+  {
+    name: 'PyLadies Tokyo',
+    logo: pyladiesTokyoImg,
+    logoAlt: 'PyLadies Tokyo Logo',
+    href: 'https://tokyo.pyladies.com/',
+  },
+  {
+    name: 'Python Asia Organization',
+    logo: pythonAsiaImg,
+    logoAlt: 'Python Asia Organization Logo',
+    href: 'https://pythonasia.org/',
+  },
+  {
+    name: 'VTC<br/>Innovation and Technology Co-creation Centre',
+    logo: vtcImg,
+    logoAlt: 'VTC Logo',
+    href: 'https://vco-create.vtc.edu.hk/en/',
   },
 ];
 
@@ -74,7 +116,7 @@ export default async function SupportingOrganizations() {
               descriptionClassName='text-sm sm:text-base text-gray-600 mt-2'
               logo={supportingOrganization.logo}
               logoAlt={supportingOrganization.logoAlt}
-              logoClassName='w-40 sm:w-64'
+              logoClassName='w-56 sm:w-80'
               href={supportingOrganization.href}
             />
           </div>
