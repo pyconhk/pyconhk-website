fb2f9dea4e02f1ed2c3c427b13fe5cf489710389
Alex Au <alexuau922@gmail.com>
2025-09-26 10:04:37 +0800

    feat(organization-logo): add modal functionality for logo display feat(sponsorships): enhance AWS sponsorship details with modal content

--- website/src/app/2025/components/utils/OrganizationLogo/index.tsx
+++ website/src/app/2025/components/utils/OrganizationLogo/index.tsx
@@ -1,8 +1,12 @@
+'use client';
 import Image, { type StaticImageData } from 'next/image';
+import { useRef } from 'react';
+import { RxCross2 } from 'react-icons/rx';
 
 export interface OrganizationLogoProps {
   name: string;
   description?: string;
+  modalNode?: React.ReactNode;
   logo: StaticImageData;
   logoAlt?: string;
   href?: string;
@@ -12,30 +16,67 @@ export interface OrganizationLogoProps {
   overallClassName?: string; // Removed as per the latest changes
 }
 
-export default async function OrganizationLogo({
+export default function OrganizationLogo({
   name,
   logo,
   href,
   description,
+  modalNode,
   logoAlt = name,
   nameClassName = '',
   descriptionClassName = '',
   logoClassName = '',
   overallClassName = '',
 }: OrganizationLogoProps) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const openDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.showModal();
+    }
+  };
   return (
     <div
       className={`flex flex-col items-center justify-center ${overallClassName}`}
     >
       <div className='skeleton w-fit h-fit'>
-        <a href={href} target='_blank' rel='noopener noreferrer'>
-          <Image
-            src={logo}
-            alt={logoAlt}
-            className={`object-contain ${logoClassName} aspect-square`}
-          />
-        </a>
+        <Image
+          src={logo}
+          alt={logoAlt}
+          className={`object-contain ${logoClassName} aspect-square ${modalNode ? 'cursor-pointer' : 'cursor-default'}`}
+          onClick={openDialog}
+        />
       </div>
+      {modalNode && (
+        <dialog id={`org-${name}`} className='modal' ref={dialogRef}>
+          <div className='modal-box w-11/12 max-w-5xl h-11/12 lg:h-23/24 bg-white text-sm md:text-base modal-middle'>
+            <form method='dialog'>
+              {/* if there is a button in form, it will close the modal */}
+              <button className='cursor-pointer absolute right-6 top-6 md:right-8 md:top-8'>
+                <RxCross2 size={24} />
+              </button>
+            </form>
+            <div className='flex items-center justify-center flex-col'>
+              <Image
+                src={logo}
+                alt={logoAlt}
+                className='object-contain aspect-square w-24 md:w-36 rounded-xl'
+              />
+              <h3 className='font-bold text-base md:text-xl'>{name}</h3>
+            </div>
+            <div className='p-4 md:p-12'>{modalNode}</div>
+            <div className='flex justify-center mt-4 mb-4 md:mb-6'>
+              <button className='btn btn-primary btn-sm md:btn-md'>
+                <a href={href} target='_blank' rel='noopener noreferrer'>
+                  Official Website
+                </a>
+              </button>
+            </div>
+          </div>
+          <form method='dialog' className='modal-backdrop'>
+            <button>close</button>
+          </form>
+        </dialog>
+      )}
       <span
         className={`text-gray-700 font-semibold text-center w-full ${nameClassName}`}
         dangerouslySetInnerHTML={{ __html: name }}
