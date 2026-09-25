cdd562b9e31a651a04cb1ead68bd1a737250df6d
skyng <skyng@hket.com>
2026-09-25 11:18:02 +0800

    feat: removed venue TBC

--- website/src/years/2026/e2e/cfp-locale-switcher.spec.ts
+++ website/src/years/2026/e2e/cfp-locale-switcher.spec.ts
@@ -79,7 +79,7 @@ test.describe('2026 conference locale switcher', () => {
         );
         await expect(page.locator('#home-title')).toHaveText('編程・連結・前行');
         await expect(page.locator('#home')).toContainText(
-          'HKIIT（將軍澳李惠利，待定）'
+          'HKIIT（將軍澳李惠利）'
         );
         await expect(page.locator('#home a[aria-label]')).toHaveAttribute(
           'aria-label',
